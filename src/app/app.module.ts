bbc4c721ae9a6b0e3505f4ea2349e8529c8a6679
a.vasilkov <a.vasilkov@voskhod.ru>
2019-10-24 17:23:11 +0300

    Дополнено README

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HotTableModule } from '@handsontable/angular';
-import { OriginService } from './origin.service'
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TableComponent } from './table/table.component';
+import { FormBuilder } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBarModule } from '@angular/material/snack-bar'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HotTableModule } from '@handsontable/angular';
 import { MaterialFileInputModule } from 'ngx-material-file-input';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { OriginService } from './origin.service'
+import { TableComponent } from './table/table.component';
+import { ResultComponent } from './result/result.component';
+import { DialogComponent } from './dialog/dialog.component';
+
 
 @NgModule({
   declarations: [
@@ -40,8 +45,4 @@ import { MaterialFileInputModule } from 'ngx-material-file-input';
 })
 export class AppModule { }
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { ResultComponent } from './result/result.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DialogComponent } from './dialog/dialog.component';
-import { FormBuilder } from '@angular/forms';
+
