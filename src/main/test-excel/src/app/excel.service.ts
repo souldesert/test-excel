546979e9ef4707cf3d68f2c7449575cf14c8ab77
a.vasilkov <a.vasilkov@voskhod.ru>
2019-11-08 14:39:11 +0300

    Change default backend endpoint

--- src/main/test-excel/src/app/excel.service.ts
+++ src/main/test-excel/src/app/excel.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ExcelRequest } from './excel-request';
@@ -15,7 +15,7 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class ExcelService {
-  apiUrl: string = "http://127.0.0.1:8080/rest/compute";  // URL REST-api
+  apiUrl: string = 'http://127.0.0.1:8080/excel-backend/compute';  // URL REST-api
 
   constructor(
     private http: HttpClient
